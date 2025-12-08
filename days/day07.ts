55ec85722f49948dd60c40aa8bd2ef461afffa09
Ryan Geddert <ryan@gddrt.com>
2025-12-08 22:39:06 +0900

    comment day 7

--- days/day07.ts
+++ days/day07.ts
@@ -7,15 +7,21 @@ async function getLines() {
 
 export async function partOne() {
     const lines = await getLines();
-    // Find the first splitter
+
+    // we don't really need to think about this graphically
+    // Iterate through the lines, if there's a beam at the same index as a splitter,
+    // then the beam splits
     const results = lines.reduce(({beams, splits}, line) => {
+        // re-create the set of beams each run, easier than mutating
         let newBeams = new Set<number>();
         for (const beam of beams) {
             if (line.charAt(beam) === "^") {
+                // If there's a splitter, the next line has a beam to the left and right of it.
                 newBeams.add(beam-1);
                 newBeams.add(beam+1);
                 splits += 1;
             } else {
+                // Otherwise, the next line has one beam at the same index.
                 newBeams.add(beam);
             }
         }
@@ -24,7 +30,7 @@ export async function partOne() {
             splits
         }
     }, {
-        beams: new Set([lines[0]!.indexOf('S')]),
+        beams: new Set([lines[0]!.indexOf('S')]), // initial beam
         splits: 0
     })
 
